47bbc226df3c4cffb1cb01b00953e63fc9be83ab
Phillip Navo <shrimp87@calvisitor-10-105-146-195.calvisitor.1918.berkeley.edu>
2017-07-26 14:49:06 -0700

    debugging

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+ 
+	{
+		"name": "Sherriff of Rock Ridge", 
+		"img": "https://upload.wikimedia.org/wikipedia/en/8/80/Cleavon_Little.jpg", 
+		"answer": [
+			"4",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"4",
+			"5"
+		]
+	}
+]
+
+module.exports = friendsArray;
